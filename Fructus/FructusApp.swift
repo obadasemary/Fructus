b97bd51cd6c93bc5f0b672790693ca520c1f21dc
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-16 02:46:24 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Abdelrahman Mohamed on 16.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
