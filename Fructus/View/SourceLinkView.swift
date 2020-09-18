e08a2da80e4ce4240b94cf97e5a05e6de4495d9c
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-18 23:51:19 +0300

    Add SourceLinkView
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ Fructus/View/SourceLinkView.swift
@@ -0,0 +1,20 @@
+//
+//  SourceLinkView.swift
+//  Fructus
+//
+//  Created by Abdelrahman Mohamed on 18.09.2020.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SourceLinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        SourceLinkView()
+    }
+}
