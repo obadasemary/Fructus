f854da6077fb712553c4c393be24b3ae7ea285c8
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-19 00:16:14 +0300

    MMMMMM
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ Fructus/App/FructusApp 2.swift
@@ -0,0 +1,24 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Abdelrahman Mohamed on 16.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    
+    @AppStorage("isOnboarding") var isOnboarding: Bool = true
+    
+    var body: some Scene {
+        WindowGroup {
+            if isOnboarding {
+                OnboardingView()
+            } else {
+                ContentView()
+            }
+        }
+    }
+}
